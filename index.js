a18560d8cc60b4a9d4d494dbd32b4908b4d5ddea
Satyasri Varaganti <satyasrib@gmail.com>
2018-10-18 06:06:07 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -13,3 +13,6 @@ function deleteFromObjectByKey(object, key) {
 function deleteFromObjectByKey(object, key) {
 
 }
+function destructivelyDeleteFromObjectByKey(object, key) {
+  delete object[ key ]
+}
